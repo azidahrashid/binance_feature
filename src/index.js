929c11c48fe1fd43e0f24d741d53539c07b97a3d
azidahrashid <turnkeyjie@gmail.com>
2025-02-11 08:26:34 +0800

    change browser router to hashrouter
    update index jsx

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {store} from './store/store';
-import  ThemeContext  from "./context/ThemeContext"; 
+import { HashRouter as Router } from "react-router-dom";  // Change BrowserRouter to HashRouter
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import ThemeContext from "./context/ThemeContext"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store = {store}>
-      <BrowserRouter basename='/'>
+    <Provider store={store}>
+      <Router> {/* Changed BrowserRouter to HashRouter */}
         <ThemeContext>
           <App />
         </ThemeContext>
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
