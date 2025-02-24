acdf89a9c66c65fcfe5968bd64313ee2e9d4f996
azidahrashid <turnkeyjie@gmail.com>
2025-02-24 10:44:37 +0800

    change tag
    table to div tag

--- src/jsx/components/Trading/Future/OpenOrdersTab.jsx
+++ src/jsx/components/Trading/Future/OpenOrdersTab.jsx
@@ -52,41 +52,41 @@ const OpenOrdersTab = () =>{
      
             <div className="table-responsive dataTabletrade " >
                 <div id="futureOpenorder_wrapper" className="dataTables_wrapper no-footer position_dataTables">
-                    <table id="example" className="table display dataTable no-footer" style={{minWidth:"845px"}}>
-                        <thead>
-                            <tr>
-                                <th><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">시간</div></div></th>
-                                <th><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">코인종류</div></div></th>
-                                <th><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">구분</div></div></th>
-                                <th><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">수량</div></div></th>
-                                <th><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">Trigger Conditions</div></div></th>
-                                <th><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center"></div></div></th>
-                            </tr>
-                        </thead>
-                        <tbody>
+                    <div id="example" className="table display dataTable no-footer" style={{minWidth:"845px"}}>
+                    <div className="thead-dataTables">
+                    <div className="th-dataTables th-dataTables_tr">
+                                <div className="th-dataTables"><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">시간</div></div></div>
+                                <div className="th-dataTables"><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">코인종류</div></div></div>
+                                <div className="th-dataTables"><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">구분</div></div></div>
+                                <div className="th-dataTables"><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">수량</div></div></div>
+                                <div className="th-dataTables"><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center">Trigger Conditions</div></div></div>
+                                <div className="th-dataTables"><div className="d-flex align-items-center "><div className=" typography-caption2 text-TertiaryText flex items-center"></div></div></div>
+                            </div>
+                        </div>
+                        <div className="tbody-dataTables">
                        
-                             <tr className="">
-                                <td><div className="d-flex align-items-center justify-content-start size_item_wrap"><span className="DateTimeWrap size_item"> <span className="dataDate">2025-01-14 </span><span className="dataTime">15:23:21</span></span></div></td>      
-                                <td className=""><div className="d-flex align-items-start justify-content-start symbol_item_wrap flex-column"><div className="symbol_item">XRPUSDT</div><div className="d-flex align-items-center"><span className="symbol_item"><span class="badge-dark light badge badge-xs badgexs_title mx-1 border-radius-5">Perp</span></span><span className="symbol_item"><span class="badge-dark light  badge badge-xs badgexs_title mx-1 border-radius-5"></span></span></div></div></td>
-                                <td><div className="d-flex align-items-center justify-content-start "><span className=" TypeOrder_item text-TakepRofitMarket">Take Profit Market</span></div></td>
-                                <td><div className="d-flex align-items-center justify-content-start "><span className="text-black AmountOrder_item">5.5659 USDT</span></div></td>
-                                <td><div className="d-flex justify-content-start flex-column"><div className="TriggerConditions_item text-TriggerConditions">표시가</div><div className="d-flex"><span className="TriggerConditions_item text-TriggerConditions"> <ChevronRight size={12} style={{marginRight:'-3px'}}/><Equal size={12} />2.9294</span></div></div></td>
+                             <div className="tr-dataTables">
+                                <div className="td-dataTables"><div className="d-flex align-items-center justify-content-start size_item_wrap"><span className="DateTimeWrap size_item"> <span className="dataDate">2025-01-14 </span><span className="dataTime">15:23:21</span></span></div></div>     
+                                <div className="td-dataTables"><div className="d-flex align-items-start justify-content-start symbol_item_wrap flex-column"><div className="symbol_item">XRPUSDT</div><div className="d-flex align-items-center"><span className="symbol_item"><span class="badge-dark light badge badge-xs badgexs_title mx-1 border-radius-5">Perp</span></span><span className="symbol_item"><span class="badge-dark light  badge badge-xs badgexs_title mx-1 border-radius-5"></span></span></div></div></div>
+                                <div className="td-dataTables"><div className="d-flex align-items-center justify-content-start "><span className=" TypeOrder_item text-TakepRofitMarket">Take Profit Market</span></div></div>
+                                <div className="td-dataTables"><div className="d-flex align-items-center justify-content-start "><span className="text-black AmountOrder_item">5.5659 USDT</span></div></div>
+                                <div className="td-dataTables"><div className="d-flex justify-content-start flex-column"><div className="TriggerConditions_item text-TriggerConditions">표시가</div><div className="d-flex"><span className="TriggerConditions_item text-TriggerConditions"> <ChevronRight size={12} style={{marginRight:'-3px'}}/><Equal size={12} />2.9294</span></div></div></div>
                                
-                                <td><div className="d-flex justify-content-between gap-2 flex-row"><button type="button" className="btn btn-success btn-xs text-uppercase flex-grow-1">수정</button><button type="button" className="btn btn-danger  btn-xs text-uppercase flex-grow-1">취소</button></div></td>
-                            </tr>
-                            <tr className="">
-                                <td><div className="d-flex align-items-center justify-content-start size_item_wrap"><span className="DateTimeWrap size_item"> <span className="dataDate">2025-01-14 </span><span className="dataTime">15:23:21</span></span></div></td>      
-                                <td className=""><div className="d-flex align-items-start justify-content-start symbol_item_wrap flex-column"><div className="symbol_item">XRPUSDT</div><div className="d-flex align-items-center"><span className="symbol_item"><span class="badge-dark light badge badge-xs badgexs_title mx-1 border-radius-5">Perp</span></span><span className="symbol_item"><span class="badge-dark light  badge badge-xs badgexs_title mx-1 border-radius-5">20x</span></span></div></div></td>
-                                <td><div className="d-flex align-items-center justify-content-start "><span className=" TypeOrder_item text-StopMarket">Stop Market</span></div></td>
-                                <td><div className="d-flex align-items-center justify-content-start "><span className="text-black AmountOrder_item">5.5659 USDT</span></div></td>
-                                <td><div className="d-flex justify-content-start flex-column"><div className="TriggerConditions_item text-TriggerConditions">표시가</div><div className="d-flex"><span className="TriggerConditions_item text-TriggerConditions"> <ChevronLeft size={12} style={{marginRight:'-3px'}}/><Equal size={12} />2.3112 </span></div></div></td>
-                                <td style={{width:'250px'}}><div className="d-flex justify-content-between gap-2 flex-row"><button type="button" className="btn btn-success btn-xs text-uppercase flex-grow-1">수정</button><button type="button" className="btn btn-danger  btn-xs text-uppercase flex-grow-1">취소</button></div></td>
-                            </tr>
+                                <div className="td-dataTables"><div className="d-flex justify-content-between gap-2 flex-row"><button type="button" className="btn btn-success btn-xs text-uppercase flex-grow-1">수정</button><button type="button" className="btn btn-danger  btn-xs text-uppercase flex-grow-1">취소</button></div></div>
+                            </div>
+                            <div className="tr-dataTables">
+                                <div className="td-dataTables"><div className="d-flex align-items-center justify-content-start size_item_wrap"><span className="DateTimeWrap size_item"> <span className="dataDate">2025-01-14 </span><span className="dataTime">15:23:21</span></span></div></div>     
+                                <div className="td-dataTables"><div className="d-flex align-items-start justify-content-start symbol_item_wrap flex-column"><div className="symbol_item">XRPUSDT</div><div className="d-flex align-items-center"><span className="symbol_item"><span class="badge-dark light badge badge-xs badgexs_title mx-1 border-radius-5">Perp</span></span><span className="symbol_item"><span class="badge-dark light  badge badge-xs badgexs_title mx-1 border-radius-5">20x</span></span></div></div></div>
+                                <div className="td-dataTables"><div className="d-flex align-items-center justify-content-start "><span className=" TypeOrder_item text-StopMarket">Stop Market</span></div></div>
+                                <div className="td-dataTables"><div className="d-flex align-items-center justify-content-start "><span className="text-black AmountOrder_item">5.5659 USDT</span></div></div>
+                                <div className="td-dataTables"><div className="d-flex justify-content-start flex-column"><div className="TriggerConditions_item text-TriggerConditions">표시가</div><div className="d-flex"><span className="TriggerConditions_item text-TriggerConditions"> <ChevronLeft size={12} style={{marginRight:'-3px'}}/><Equal size={12} />2.3112 </span></div></div></div>
+                                <div className="td-dataTables" style={{width:'250px'}}><div className="d-flex justify-content-between gap-2 flex-row"><button type="button" className="btn btn-success btn-xs text-uppercase flex-grow-1">수정</button><button type="button" className="btn btn-danger  btn-xs text-uppercase flex-grow-1">취소</button></div></div>
+                            </div>
 
                           
                                                     
-                        </tbody>
-                    </table>
+                        </div>
+                    </div>
                     {/* <div className="d-sm-flex text-center justify-content-between align-items-center mt-3 mb-3">
                         <div className="dataTables_info">
                             Showing {activePag.current * sort + 1} to{" "}
