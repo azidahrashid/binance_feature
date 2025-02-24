e5df4704c38e5a8792cb0f1d5703c3318e530c8d
azidahrashid <turnkeyjie@gmail.com>
2025-02-24 16:19:23 +0800

    update css
    update style

--- src/jsx/components/Modal/ModalTPSL.jsx
+++ src/jsx/components/Modal/ModalTPSL.jsx
@@ -39,7 +39,7 @@ const ModalTPSL = () => {
       >
             <h3 className="text-xl text-SecondaryText fw-bold mb-4"></h3>
             <div className="my-2 d-flex justify-content-between gap-1  align-items-center"><div className="SecondaryBetweenText"><span className="text-muted  mb-4">코인종류</span></div><div className="SecondaryBetweenText"><span className="text-black mx-2">XRPUSDT</span><span className="text-black ">Perpetual</span></div></div>
-            <div className="my-2 d-flex justify-content-between gap-1  align-items-center"><div className="SecondaryBetweenText"><span className="text-muted  mb-4">코인종류</span></div><div className="SecondaryBetweenText"><span className="text-black mx-2">XRPUSDT</span><span className="text-black ">Perpetual</span></div></div>
+            <div className="my-2 d-flex justify-content-between gap-1  align-items-center"><div className="SecondaryBetweenText"><span className="text-muted  mb-4">진입가격</span></div><div className="SecondaryBetweenText"><span className="text-black mx-2">XRPUSDT</span><span className="text-black ">Perpetual</span><span className="text-black ">123,456 USDT</span></div></div>
            
            
             {[
