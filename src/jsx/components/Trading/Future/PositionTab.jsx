b582ed9401807156100cce882fb0dce146cc0a0a
azidahrashid <turnkeyjie@gmail.com>
2025-02-24 12:56:47 +0800

    change class name
    width change

--- src/jsx/components/Trading/Future/PositionTab.jsx
+++ src/jsx/components/Trading/Future/PositionTab.jsx
@@ -57,29 +57,29 @@ const PositionTab = () =>{
                         <div className="thead-dataTables">
                              <div className="th-dataTables th-dataTables_tr">
                                  <div className="th-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">코인종류</div></div></div>
-                                 <div className="th-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">사이즈</div></div></div>
+                                 <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">사이즈</div></div></div>
                                  <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">진입가</div></div></div>
                                  <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">현재가</div></div></div>
                                  <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">청산가</div></div></div>
-                                 <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">마진율</div></div></div>
+                                 <div className="th-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">마진율</div></div></div>
                                  <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">증거금</div></div></div>
                                  <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">미실현수익(수익률 %)</div></div></div>
-                                 <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-closePosition d-flex items-center justify-content-center w-full">포지션종료</div></div></div>
-                                 <div className="th-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">TP/SL</div></div></div>
+                                 <div className="th-dataTables table-w-3"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-closePosition d-flex items-center justify-content-center w-full">포지션종료</div></div></div>
+                                 <div className="th-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill"><div className=" typography-caption2 text-TertiaryText d-flex items-center justify-content-center w-full">TP/SL</div></div></div>
                             </div>
                         </div>
                         <div className="tbody-dataTables">
                        
                               <div className="tr-dataTables tdGreen">
                                  <div className="td-dataTables line_tag table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill symbol_item_wrap flex-column"><div className="symbol_item">XRPUSDT</div><div className="d-flex align-items-center"><span className="symbol_item"><span class="badge-dark light badge badge-xs badgexs_title mx-1 border-radius-5">Perp</span></span><span className="symbol_item"><span class="badge-dark light  badge badge-xs badgexs_title mx-1 border-radius-5">20x</span></span></div></div></div>
-                                 <div className="td-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill size_item_wrap"><span className="text-size-trade size_item">9.6603 USDT</span></div></div>
+                                 <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill size_item_wrap"><span className="text-size-trade size_item">9.6603 USDT</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black EntryPrice_item">2.35326</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarkPrice_item">2.35366</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-mark-price LiqPrice_item">1.2345</span></div></div>
-                                 <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarginRatio_item">0.10%</span></div></div>
+                                 <div className="td-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarginRatio_item">0.10%</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-column align-items-center"><div className="margin_item text-margin">0.48 USDT</div><div className="d-flex"><span className="margin_item text-margin"> (Cross) </span></div></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-column align-items-center"><div className="PNLROI_item text-PNLROI-plus">+0.01 USDT</div><div className="d-flex"><span className="PNLROI_item text-PNLROI"> (<Plus size={12} />3.39%) </span></div></div></div>
-                                 <div className="td-dataTables table-w-2 td_closePositionWrap">
+                                 <div className="td-dataTables table-w-3 td_closePositionWrap">
                                     <div className="d-flex justify-content-center flex-fill flex-row"><div className="closePositionWrap d-flex justify-content-center flex-fill flex-row">
                                         <span className="text-closePositionMarket text-closePosition">Market</span><span className="text-borderline mx-2">|</span>
                                         <span className="text-closePositionLimit text-black"> Limit </span></div>
@@ -107,20 +107,20 @@ const PositionTab = () =>{
                                             </div>
                                         </div>
                                  </div>
-                                 <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-row"><ModalTPSL/></div></div>
+                                 <div className="td-dataTables table-w-1"><div className="d-flex justify-content-center flex-fill flex-row"><ModalTPSL/></div></div>
                            
                                 </div>
 
                              <div className="tr-dataTables tdGreen">
                                  <div className="td-dataTables line_tag table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill symbol_item_wrap flex-column"><div className="symbol_item">XRPUSDT</div><div className="d-flex align-items-center"><span className="symbol_item"><span class="badge-dark light badge badge-xs badgexs_title mx-1 border-radius-5">Perp</span></span><span className="symbol_item"><span class="badge-dark light  badge badge-xs badgexs_title mx-1 border-radius-5">20x</span><span class="badge-green light badge badge-xs badgexs_title mx-1 border-radius-5">Long</span></span></div></div></div>
-                                 <div className="td-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill size_item_wrap"><span className="text-size-trade size_item">9.6603 USDT</span></div></div>
+                                 <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill size_item_wrap"><span className="text-size-trade size_item">9.6603 USDT</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black EntryPrice_item">2.35326</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarkPrice_item">2.35366</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-mark-price LiqPrice_item">1.2345</span></div></div>
-                                 <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarginRatio_item">0.10%</span></div></div>
+                                 <div className="td-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarginRatio_item">0.10%</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-column align-items-center"><div className="margin_item text-margin">0.48 USDT</div><div className="d-flex"><span className="margin_item text-margin"> (Cross) </span></div></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-column align-items-center"><div className="PNLROI_item text-PNLROI-plus"><Plus size={12} />0.01 USDT</div><div className="d-flex"><span className="PNLROI_item text-PNLROI"> (<Plus size={12} />3.39%) </span></div></div></div>
-                                 <div className="td-dataTables table-w-2 td_closePositionWrap"><div className="d-flex justify-content-center flex-fill flex-row"><div className="closePositionWrap d-flex justify-content-center flex-fill flex-row"><span className="text-closePositionMarket text-closePosition">Market</span><span className="text-borderline mx-2">|</span><span className="text-closePositionLimit text-black"> Limit </span></div>
+                                 <div className="td-dataTables table-w-3 td_closePositionWrap"><div className="d-flex justify-content-center flex-fill flex-row"><div className="closePositionWrap d-flex justify-content-center flex-fill flex-row"><span className="text-closePositionMarket text-closePosition">Market</span><span className="text-borderline mx-2">|</span><span className="text-closePositionLimit text-black"> Limit </span></div>
                                         <div className="closePositionWrap d-flex justify-content-center flex-fill flex-row closePosition_editCol mx-5">
                                                <DropType 
                                                         currentValue="2.5336"
@@ -144,20 +144,20 @@ const PositionTab = () =>{
                                             </div>
                                         </div>
                                 </div>
-                                 <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-row"><ModalTPSL/></div></div>
+                                 <div className="td-dataTables table-w-1"><div className="d-flex justify-content-center flex-fill flex-row"><ModalTPSL/></div></div>
                             </div>
 
                             
                              <div className="tr-dataTables tdRed">
                                  <div className="td-dataTables line_tag table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill symbol_item_wrap flex-column"><div className="symbol_item">XRPUSDT</div><div className="d-flex align-items-center"><span className="symbol_item"><span class="badge-dark light badge badge-xs badgexs_title mx-1 border-radius-5">Perp</span><span class="badge-dark light  badge badge-xs badgexs_title mx-1 border-radius-5">20x</span><span class="badge-red badge badge-xs badgexs_title mx-1 border-radius-5">Short</span></span><span className="symbol_item"></span></div></div></div>
-                                 <div className="td-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill size_item_wrap"><span className="text-size-trade-minus size_item"><Minus size={12} />9.7269 USDT</span></div></div>
+                                 <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill size_item_wrap"><span className="text-size-trade-minus size_item"><Minus size={12} />9.7269 USDT</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black EntryPrice_item">2.35326</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarkPrice_item">2.35366</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-mark-price LiqPrice_item">1.2345</span></div></div>
-                                 <div className="td-dataTables table-w-2"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarginRatio_item">0.10%</span></div></div>
+                                 <div className="td-dataTables table-w-1"><div className="d-flex align-items-center justify-content-center flex-fill "><span className="text-black MarginRatio_item">0.10%</span></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-column align-items-center"><div className="margin_item text-margin">0.48 USDT</div><div className="d-flex"><span className="margin_item text-margin"> (Cross) </span></div></div></div>
                                  <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-column align-items-center"><div className="PNLROI_item text-PNLROI-minus"><Minus size={12} />0.01 USDT</div><div className="d-flex"><span className="PNLROI_item text-PNLROI"> (<Plus size={12} />3.39%) </span></div></div></div>
-                                 <div className="td-dataTables table-w-2 td_closePositionWrap"><div className="d-flex justify-content-center flex-fill flex-row"><div className="closePositionWrap d-flex justify-content-center flex-fill flex-row"><span className="text-closePositionMarket text-closePosition">Market</span><span className="text-borderline mx-2">|</span><span className="text-closePositionLimit text-black"> Limit </span></div>
+                                 <div className="td-dataTables table-w-3 td_closePositionWrap"><div className="d-flex justify-content-center flex-fill flex-row"><div className="closePositionWrap d-flex justify-content-center flex-fill flex-row"><span className="text-closePositionMarket text-closePosition">Market</span><span className="text-borderline mx-2">|</span><span className="text-closePositionLimit text-black"> Limit </span></div>
                                         <div className="closePositionWrap d-flex justify-content-center flex-fill flex-row closePosition_editCol mx-5">
                                                <DropType 
                                                         currentValue="2.5336"
@@ -182,7 +182,7 @@ const PositionTab = () =>{
                                             </div>
                                         </div>
                                 </div>
-                                 <div className="td-dataTables table-w-2"><div className="d-flex justify-content-center flex-fill flex-row"><ModalTPSL/></div></div>
+                                 <div className="td-dataTables table-w-1"><div className="d-flex justify-content-center flex-fill flex-row"><ModalTPSL/></div></div>
                             </div>
                                                        
                         </div>
